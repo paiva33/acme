79828c69f81cc2f1e3a7141e8f599d3ff9ad3bed
paiva <paiva@localhost.localdomain>
2020-11-25 09:14:26 -0300

    Adicionando component ListTarefasComponent

---
+++ acme-ui/src/modulos/tarefas/ListTarefasComponent.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class ListTarefasComponent extends React.Component {
+
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            tarefas: []
+        }
+    }
+
+
+    refreshTodos() {
+        
+    }
+}
