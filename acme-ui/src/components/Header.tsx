a7169afb30dea3ea4e2e16f37e5386461148334e
Charles Paiva <paiva.charles@gmail.com>
2020-11-25 06:41:56 -0300

    Adicionando bootswatch

---
+++ acme-ui/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header = () => 
+    <header>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+            <div><a href="http://www.in28minutes.com" className="navbar-brand">in28Minutes</a></div>
+            <ul className="navbar-nav">
+                <li><a>Home</a></li>
+                <li><a>Todos</a></li>
+            </ul>            
+        </nav>
+    </header>
