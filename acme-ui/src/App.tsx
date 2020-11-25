095959d7b01aa7d0167c8bfb31909dee54fad09c
Charles Paiva <paiva.charles@gmail.com>
2020-11-24 21:50:51 -0300

    Adicionando reactstrap

--- acme-ui/src/App.tsx
+++ acme-ui/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'reactstrap';
 import logo from './logo.svg';
 import './App.scss';
 
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button color="danger">Danger!</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
